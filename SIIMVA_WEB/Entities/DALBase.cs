a4e967dcac2e37cce1fb287c39b9edb8b1334e42
sistemasvillaallende <107209701+sistemasvillaallende@users.noreply.github.com>
2025-04-22 12:08:43 -0300

    Stop Traking

--- SIIMVA_WEB/Entities/DALBase.cs
+++ SIIMVA_WEB/Entities/DALBase.cs
@@ -29,7 +29,7 @@ namespace MOTOR_WORKFLOW.Entities
         {
             try
             {
-                return new SqlConnection("Data Source=10.0.0.8;Initial Catalog=SIIMVA;User ID=general;");
+                return new SqlConnection("Data Source=10.0.0.23;Initial Catalog=SIIMVA;User ID=general;");
             }
             catch (Exception ex)
             {
